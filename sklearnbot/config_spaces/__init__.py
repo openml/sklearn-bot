cd56625c3fd69b9c923e97c708d994abc24855c3
janvanrijn <janvanrijn@gmail.com>
2018-10-08 13:20:05 -0400

    added mlp import

--- sklearnbot/config_spaces/__init__.py
+++ sklearnbot/config_spaces/__init__.py
@@ -1,6 +1,7 @@
 # contents of this library should be moved to other library
 import sklearnbot.config_spaces.adaboost
 import sklearnbot.config_spaces.decision_tree
+import sklearnbot.config_spaces.neural_network
 import sklearnbot.config_spaces.random_forest
 import sklearnbot.config_spaces.svc
 from .bootstrap import get_available_config_spaces, get_config_space
